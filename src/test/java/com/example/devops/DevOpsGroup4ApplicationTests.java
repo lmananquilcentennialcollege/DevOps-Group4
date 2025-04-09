00bfa25d47f95c1baee72936bc0d13a9c396eb8c
Donalda Zhuga <dzhuga@my.centennialcollege.ca>
2025-04-09 17:03:31 -0400

    Initial commit

---
+++ src/test/java/com/example/devops/DevOpsGroup4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGroup4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
