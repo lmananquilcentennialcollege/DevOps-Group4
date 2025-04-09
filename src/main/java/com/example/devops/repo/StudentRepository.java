79758a808ff4337a391f4ebfbf6629bff86d6a09
Donalda Zhuga <dzhuga@my.centennialcollege.ca>
2025-04-09 18:46:38 -0400

    Update StudentRepository

--- src/main/java/com/example/devops/repo/StudentRepository.java
+++ src/main/java/com/example/devops/repo/StudentRepository.java
@@ -1,11 +1,8 @@
 package com.example.devops.repo;
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.devops.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-	Optional<Student> findByUsername(String username);
+	Optional<Student> findByUsername(String username); //StudentRepository
 }
