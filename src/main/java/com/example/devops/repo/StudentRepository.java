4b73f5f80d15584148cedd0c3f22ec5c8363efe2
Donalda Zhuga <dzhuga@my.centennialcollege.ca>
2025-04-09 18:41:55 -0400

    Created StudentRepository.java

--- src/main/java/com/example/devops/repo/StudentRepository.java
+++ src/main/java/com/example/devops/repo/StudentRepository.java
@@ -1,5 +1,11 @@
 package com.example.devops.repo;
 
-public class StudentRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.devops.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	Optional<Student> findByUsername(String username);
 }
